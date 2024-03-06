160973ec19d67016962d6277bacfeda8ec613ebf
BIThooven <yassine.parkourist@gmail.com>
2024-03-06 21:34:09 +0100

    web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """distruibutes an archive to my web servers"""
-from fabric.api import env, put, run, local
+from fabric.api import *
 from datetime import datetime
 import os
 
@@ -28,22 +28,19 @@ def do_deploy(archive_path):
     """distributes an archive to my web servers"""
     if not os.path.exists(archive_path):
         return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        file_name = archive_path.split('/')[1]
-        no_ext = file_name.split('.')[0]
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(no_ext))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, no_ext))
-        run("sudo rm /tmp/{}".format(file_name))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(no_ext, no_ext))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(no_ext))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(no_ext))
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-    except Exception:
+    except:
         return False
